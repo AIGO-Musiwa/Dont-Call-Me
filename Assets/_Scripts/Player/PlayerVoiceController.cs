5920d37d8d28c38544751979d4aee364b5b4ec19
onikatashi <llho717@naver.com>
2026-05-12 22:49:37 +0900

    이젠 진짜 관전자 문제 없을거임

--- Assets/_Scripts/Player/PlayerVoiceController.cs
+++ Assets/_Scripts/Player/PlayerVoiceController.cs
@@ -153,7 +153,7 @@ public class PlayerVoiceController : NetworkBehaviour
         if (playerController.NetPlayerState == PlayerState.Dead ||
             playerController.NetPlayerState == PlayerState.Escaped)
         {
-            VoiceManager.Instance?.SwitchToSpectatorMode(playerController.NetZone);
+            VoiceManager.Instance?.SetSpectatotrSendGroup();
         }
     }
 
