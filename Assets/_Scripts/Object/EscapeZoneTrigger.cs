9e97205b272b61c2fcf837fd62d3ac65792a65ad
Siwoone <245989614+Siwoone@users.noreply.github.com>
2026-05-19 09:58:44 +0900

    commit2

--- Assets/_Scripts/Object/EscapeZoneTrigger.cs
+++ Assets/_Scripts/Object/EscapeZoneTrigger.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
-using Unity.VisualScripting;
 
 [RequireComponent(typeof(BoxCollider))]
 public class EscapeZoneTrigger : MonoBehaviour
